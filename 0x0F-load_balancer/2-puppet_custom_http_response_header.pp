6ca988bf15e96e64565e42b9dc7b590ea72e49eb
uelkariuki <cratzuel@gmail.com>
2023-09-05 06:19:40 +0000

    fix Task 2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,14 +2,31 @@
 # The name of the custom HTTP header must be X-Served-By
 # The value of the custom HTTP header must be the hostname of the server Nginx is running on
 # Write 2-puppet_custom_http_response_header.pp so that it configures a brand new Ubuntu machine to the requirements asked in this task
-# -i option is to add in place to make changes to the input file directly instead of printing result to stdout
-# /a option is to append line that adds a custom HTTP header after the line listen 80 default_server
-# /listen .. the / helps know which pattern or line that is being looked for
+#
 
-exec { 'add_header':
-  command => 'echo "    add_header X-Served-By $(hostname);" | sudo tee /tmp/add_header.txt > /dev/null;
-        sudo sed -i "/listen 80 default_server/r /tmp/add_header.txt" /etc/nginx/sites-available/default;
-        sudo rm /tmp/add_header.txt;
-        sudo service nginx restart',
-  provider => shell,
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+  path    =>  ['/usr/bin', '/bin']
+}
+
+
+# install nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# add X-Served-By custom header
+file_line { 'add header':
+  ensure => present,
+  path =>  '/etc/nginx/sites-available/default',
+  line => "    add_header X-Served-By ${hostname};",
+  after => 'index index.html;',
+}
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
