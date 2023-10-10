2d667b804cc0bca1e64f1116ccd1ddf9400d017b
uelkariuki <cratzuel@gmail.com>
2023-10-10 17:03:43 +0000

    Add a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -17,17 +17,30 @@ def helper_recurse(posts):
     return [posts[0]['data']['title']] + helper_recurse(posts[1:])
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """
     Queries the Reddit API and returns a list containing the titles
     of all hot articles for a given subreddit
     """
-    headers = {'User-agent': '1-top_ten/1.0 (fipis92205@dixiser.com)'}
-    response = requests.get(f'https://www.reddit.com/r/{subreddit}/\
-hot.json', headers=headers)
+    headers = {'User-agent': '2-recurse/1.0 (fipis92205@dixiser.com)'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+
+    # to handle pagination that the Reddit API uses
+    # add 'after' parameter to the url if it is not none
+    if after:
+        url += f'?after={after}'
+
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        total_posts = response.json()['data']['children']
-        hot_list = helper_recurse(total_posts)
+        total_data = response.json()['data']
+        total_posts = total_data['children']
+        hot_list += helper_recurse(total_posts)
+
+        # if more data is present call the function recursively
+        # with the 'after' parameter
+        if total_data['after']:
+            return recurse(subreddit, hot_list, total_data['after'])
+
         return hot_list
     else:
         return None
