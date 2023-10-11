902987113d39ce63609cdf6f34fbe141969ca54b
uelkariuki <cratzuel@gmail.com>
2023-10-11 02:21:53 +0000

    Fix: Add a recursive function that queries the Reddit API, parses the title of all hot articles and prints a sorted count of given keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,42 +7,53 @@ javascript, but java should not).
 """
 
 import requests
-import re
-from collections import Counter
 
 
-def count_words(subreddit, word_list, after=None, counter=None):
+def count_words(subreddit, word_list, after="", counter=0, instances={}):
     """
     Queries the Reddit API, parses the title of all hot articles,
     and prints a sorted count of given keywords (case-insensitive,
     delimited by spaces.
     """
-    if counter is None:
-        counter = Counter()
-
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
     headers = {'User-agent': '100-count/1.0 (fipis92205@dixiser.com)'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    params = {"limit": 100}
-    if after:
-        params["after"] = after
+    params = {"after": after, "counter": counter, "limit": 100}
 
-    response = requests.get(url, headers=headers, params=params)
-    all_data = response.json()
-    if (response.status_code == 200 and "data" in all_data
-            and "children" in all_data["data"]):
-        for post in all_data["data"]["children"]:
-            title = post["data"]["title"].lower()
-            for word in word_list:
-                counter[word] += len(re.findall(rf"\b{word}\b", title))
-    else:
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    try:
+        all_data = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
         return
+    all_data = response.json()
 
-    if "after" in all_data["data"] and all_data["data"]["after"]:
-        count_words(subreddit, word_list,
-                    after=all_data["data"]["after"], counter=counter)
+    data = all_data.get("data")
+    counter += data.get("dist")
+    children = data.get("children")
+    after = data.get("after")
+
+    for children_result in children:
+        title = children_result.get("data").get("title").lower().split()
+        for word in word_list:
+            word = word.lower()
+            no_times = title.count(word)
+            if no_times > 0:
+                instances[word] = instances.get(word, 0) + no_times
+
+    if after is None:
+        if not instances:
+            print("")
+        else:
+            sort_instances = sorted(
+                                    instances.items(), key=lambda
+                                    key_value: (key_value[1],
+                                                key_value[0])
+                                    )
+            for key, value in sort_instances:
+                print(f"{key}: {value}")
     else:
-        count_sorted = sorted([(key, value) for key, value in
-                              counter.items() if value > 0],
-                              key=lambda x: (-x[1], x[0]))
-        for word, count in count_sorted:
-            print(f"{word}: {count}")
+        count_words(subreddit, word_list, after, counter, instances)
