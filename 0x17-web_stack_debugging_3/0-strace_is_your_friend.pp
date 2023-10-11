9e1dff7f43ccaed421d3ea97599ebef837150815
uelkariuki <cratzuel@gmail.com>
2023-10-11 22:40:34 +0000

    Automate fix of Apache returning a 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,7 @@
-# Automate fix of Apache returning a 500 error
-file { '/var/www/html/index.html':
-  ensure => 'present'
+# Automate fix of Apache returning a 500 error by fixing wrong .phpp extension to .php
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+
 }
