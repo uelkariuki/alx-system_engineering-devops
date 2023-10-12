a66dd82c9b0b08bd0a4acf8103a1c0cab75c54fe
uelkariuki <cratzuel@gmail.com>
2023-10-12 13:02:15 +0000

    Fix: recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,45 +9,42 @@ javascript, but java should not).
 import requests
 
 
-def count_words(subreddit, word_list, instances={}, after=None, count=0):
+def count_words(subreddit, word_list, after=None, results=None):
     """
     Queries the Reddit API, parses the title of all hot articles,
     and prints a sorted count of given keywords (case-insensitive,
     delimited by spaces.
     """
-    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
-    headers = {'User-agent': '100-count/1.0 (fipis92205@dixiser.com)'}
-    params = {"after": after, "count": count, "limit": 100}
+    if results is None:
+        results = {}
+
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-agent': '100-count/1.0'}
+    params = {"after": after}
 
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    try:
-        results = response.json()
-        if response.status_code == 404 or results is None:
-            raise Exception
-    except Exception:
-        print("")
+    if (response.status_code != 200):
         return
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
 
-    for children_result in results.get("children"):
-        title = children_result.get("data").get("title").lower().split()
+    data = response.json()['data']
+    titles = [post['data']['title'] for post in data['children']]
+
+    for children_result in titles:
+        title = children_result.lower().split()
+
         for word in word_list:
-            if word.lower() in title:
-                no_times = title.count(word.lower())
-                instances[word] = instances.get(word, 0) + no_times
+            no_times = title.count(word)
+            if (no_times > 0):
+                results[word] = results.get(word, 0) + no_times
 
-    if after is None:
-        if not instances:
-            print("")
-        else:
-            sort_instances = sorted(
-                    instances.items(),
-                    key=lambda key_value: (-key_value[1], key_value[0])
-                    )
-            [print(f"{key}: {value}") for key, value in sort_instances]
+    if not data['after']:
+        sort_instances = sorted(
+                results.items(),
+                key=lambda x: (-x[1], x[0].lower()))
+        for word, count in sort_instances:
+            if (count > 0):
+                print(f"{word.lower()}: {count}")
     else:
-        count_words(subreddit, word_list, instances, after, count)
+        return count_words(subreddit, word_list,
+                           after=data['after'], results=results)
