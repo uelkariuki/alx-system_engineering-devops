0eb49bb8be7c5996c704df35fcf27c37f6282b50
uelkariuki <cratzuel@gmail.com>
2023-10-19 18:25:42 +0000

    Fix the huge amount of Nginx failed requests from being under pressure

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,12 @@
 # Increasing the ULIMIT of default file from 15 to 4096 to handle more requests and fix the failing requests
 
 
-exec { 'fix-nginx':
+exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
+} ->
 
-} - >
-
-# restart nginx
-exec { 'restart-nginx':
+exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
